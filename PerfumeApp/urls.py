2c587e83b8c17b95dd29a2dce49683f6c4059367
hallak25 <74534172+hallak25@users.noreply.github.com>
2025-03-12 12:47:28 +0400

    Add files via upload

--- PerfumeApp/urls.py
+++ PerfumeApp/urls.py
@@ -6,8 +6,8 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('', views.welcome_view, name='main'),
-    path('catalog/', views.catalog_view, name='catalog'),
+    path('', views.welcome_view_ru, name='main'),
+    path('catalog/', views.catalog_view_ru, name='catalog'),
     path('ru/', views.welcome_view_ru, name='main_ru'),
     path('catalog_ru/', views.catalog_view_ru, name='catalog_ru'),
     path('welcome/', views.welcome_view, name='welcome'),
